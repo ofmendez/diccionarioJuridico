e1f0d964c0541a292a232baf5af0e22c47bc8678
Andres Garcia <andresgarcia@tretto.co>
2023-10-05 16:09:41 -0500

    Actualizacion Front Admin

---
+++ FrontAdmin/JS/JS.js
@@ -0,0 +1,50 @@
+// Definición Medida Ancho Menu Lateral
+function definirMedidaMenuLateral() {
+    var anchoMenuLateral = document.getElementById('MenuLateral').clientWidth;
+    if (window.innerWidth > 1250) {
+        document.documentElement.style.setProperty('--SeccionDerechaTamanoAutomatico', window.innerWidth - anchoMenuLateral + 'px');
+        document.documentElement.style.setProperty('--SeccionesDerechaMenuAncho', anchoMenuLateral + 'px');
+    } else if (window.innerWidth > 992) {
+        document.documentElement.style.setProperty('--SeccionesDerechaMenuAncho', anchoMenuLateral + 'px');
+    } else if (window.innerWidth < 991) {
+        document.documentElement.style.setProperty('--SeccionesDerechaMenuAncho', '0px');
+    }
+ }
+ definirMedidaMenuLateral();
+ addEventListener("resize", (event) => {
+    definirMedidaMenuLateral();
+ });
+
+ // Funacionamientos Menús
+function abrirMenuLateral() {
+    document.getElementById('MenuLateral').classList.add('slide-in');
+    document.getElementById('MenuLateral').classList.remove('slide-out');
+    document.getElementById('OverlayMenu').classList.add('overlay-in');
+    document.getElementById('OverlayMenu').classList.remove('overlay-out');
+    document.getElementById('MenuConfiguracion').classList.add('slide-down');
+    document.getElementById('MenuConfiguracion').classList.remove('slide-up');
+}
+
+function cerrarMenuLateral() {
+    document.getElementById('MenuLateral').classList.add('slide-out');
+    document.getElementById('MenuLateral').classList.remove('slide-in');
+    document.getElementById('OverlayMenu').classList.add('overlay-out');
+    document.getElementById('OverlayMenu').classList.remove('overlay-in');
+    document.getElementById('MenuConfiguracion').classList.add('slide-down');
+    document.getElementById('MenuConfiguracion').classList.remove('slide-up');
+    document.getElementById('ContenedorMenuConfiguracion').classList.add('z-index-down');
+    document.getElementById('ContenedorMenuConfiguracion').classList.remove('z-index-up');
+}
+
+// Definición Altura Contenido Home
+function definirAlturaMensajeEstadisticas() {
+    var alturaMensajeEstadisticas = document.getElementById('SeccionMensajeEstadisticas').clientHeight;
+    var valorPadding = getComputedStyle(document.documentElement).getPropertyValue('--PaddingElementos').replace("px", "");
+    document.getElementById('SeccionContenidoHome').style.height = (window.innerHeight - (alturaMensajeEstadisticas + (valorPadding * 2))) + 'px';
+}
+
+definirAlturaMensajeEstadisticas();
+addEventListener("resize", (event) => {
+    definirAlturaMensajeEstadisticas();
+});
+
